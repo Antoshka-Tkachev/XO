564f06b7d5f9f6c8c17e1bb8c16b261f08f191c7
Antoshka <tosh234@mail.ru>
2019-08-10 04:04:27 +0300

    Alpha test

--- XO/XO/XO.cpp
+++ XO/XO/XO.cpp
@@ -271,10 +271,10 @@ public:
 		return complete;
 	}
 
-	int attack()
+	int attack_O()
 	{
 		int bufer_i = -1, bufer_j = -1; // для хранения соседнего элемента
-		for (int i = 0; i < 3; i++) // ищем Х
+		for (int i = 0; i < 3; i++) // ищем O
 		{
 			for (int j = 0; j < 3; j++)
 			{
@@ -413,7 +413,7 @@ public:
 		return 2;
 	}
 
-	int defense()
+	int defense_O()
 	{
 		int bufer_i = -1, bufer_j = -1; // для хранения соседнего элемента
 		for (int i = 0; i < 3; i++) // ищем Х
@@ -581,6 +581,318 @@ public:
 		}
 		return 1;
 	}
+
+	int attack_X()
+	{
+		int bufer_i = -1, bufer_j = -1; // для хранения соседнего элемента
+		for (int i = 0; i < 3; i++) // ищем Х
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (arr[i][j] == 'X')
+				{
+
+					if (!(i == 1 && j == 1) && arr[1][1] == 'X') //если не в центре, но есть центральный элемент, по переводим на ситуацию "центр"
+						goto center;
+
+					if (i == 1 && j == 1) // если он в центре то ищем "Соседа", пропуская центр
+					{
+					center:
+						for (int q = 0; q < 3; q++)
+						{
+							for (int p = 0; p < 3; p++)
+							{
+								if (!(q == 1 && p == 1) && arr[q][p] == 'X')
+								{
+									bufer_i = q; //запоминаем индексы "Соседа"
+									bufer_j = p;
+
+									bufer_i = 1 - bufer_i; //находим "шаг"
+									bufer_j = 1 - bufer_j;
+									bufer_i = 1 + bufer_i; //находим индексы "недостоющей до тройки" ячейки
+									bufer_j = 1 + bufer_j;
+									if (arr[bufer_i][bufer_j] == ' ')//если эта ячейка пустая, то запоняем
+									{
+										arr[bufer_i][bufer_j] = 'X';
+										return 1;
+									}
+								}
+							}
+						}
+					}
+
+					if (i != 1 && j != 1 && arr[i][j] == 'X') //если Х в углу, то ищем пару
+					{
+						int count = 0; //cчетчик кол-ва угловых элементов
+
+						for (int q = 0; q < 3; q += 2) //считаем кол-во угловых элементов
+							for (int p = 0; p < 3; p += 2)
+								if (arr[q][p] == 'X')
+									count++;
+
+						if (count > 1) // если элементов 2 и больше, то ищем пары
+						{
+							if ((arr[0][0] == 'X' && arr[2][2] == 'X') || (arr[0][2] == 'X' && arr[2][0] == 'X')) //если пара диагональна
+							{
+								if (arr[1][1] == ' ') // и центр свободен, то ставим в центр
+								{
+									arr[1][1] = 'X';
+									return 1;
+								}
+							}
+
+							//4 варианта соседих углов
+							// если есть пара и элемент между ними свободен, то делаем этот ход
+							if (arr[0][0] == 'X' && arr[0][2] == 'X' && arr[0][1] == ' ')
+							{
+								arr[0][1] = 'X';
+								return 1;
+							}
+							if (arr[0][0] == 'X' && arr[2][0] == 'X' && arr[1][0] == ' ')
+							{
+								arr[1][0] = 'X';
+								return 1;
+							}
+							if (arr[2][0] == 'X' && arr[2][2] == 'X' && arr[2][1] == ' ')
+							{
+								arr[2][1] = 'X';
+								return 1;
+							}
+							if (arr[0][2] == 'X' && arr[2][2] == 'X' && arr[1][2] == ' ')
+							{
+								arr[1][2] = 'X';
+								return 1;
+							}
+						}
+					}
+
+					if ((arr[1][0] == 'X' && arr[1][2] == 'X') || (arr[0][1] == 'X' && arr[2][1] == 'X')) //если пара противоположная
+					{
+						if (arr[1][1] == ' ') // и центр свободен, то ставим в центр
+						{
+							arr[1][1] = 'X';
+							return 1;
+						}
+					}
+
+					//8 ситуаций на соседние элементы
+					if (arr[0][0] == 'X' && arr[0][1] == 'X' &&  arr[0][2] == ' ')
+					{
+						arr[0][2] = 'X';
+						return 1;
+					}
+					if (arr[0][1] == 'X' && arr[0][2] == 'X' &&  arr[0][0] == ' ')
+					{
+						arr[0][0] = 'X';
+						return 1;
+					}
+					if (arr[0][2] == 'X' && arr[1][2] == 'X' &&  arr[2][2] == ' ')
+					{
+						arr[2][2] = 'X';
+						return 1;
+					}
+					if (arr[1][2] == 'X' && arr[2][2] == 'X' &&  arr[0][2] == ' ')
+					{
+						arr[0][2] = 'X';
+						return 1;
+					}
+					if (arr[2][2] == 'X' && arr[2][1] == 'X' &&  arr[2][0] == ' ')
+					{
+						arr[2][0] = 'X';
+						return 1;
+					}
+					if (arr[2][0] == 'X' && arr[2][1] == 'X' &&  arr[2][2] == ' ')
+					{
+						arr[2][2] = 'X';
+						return 1;
+					}
+					if (arr[0][0] == 'X' && arr[1][0] == 'X' &&  arr[2][0] == ' ')
+					{
+						arr[2][0] = 'X';
+						return 1;
+					}
+					if (arr[1][0] == 'X' && arr[2][0] == 'X' &&  arr[0][0] == ' ')
+					{
+						arr[0][0] = 'X';
+						return 1;
+					}
+
+					return 2;
+				}
+			}
+		}
+		return 2;
+	}
+
+	int defense_X()
+	{
+		int bufer_i = -1, bufer_j = -1; // для хранения соседнего элемента
+		for (int i = 0; i < 3; i++) // ищем O
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (arr[i][j] == 'O')
+				{
+
+					if (!(i == 1 && j == 1) && arr[1][1] == 'O') //если не в центре, но есть центральный элемент, по переводим на ситуацию "центр"
+						goto center;
+
+					if (i == 1 && j == 1) // если он в центре то ищем "Соседа", пропуская центр
+					{
+					center:
+						for (int q = 0; q < 3; q++)
+						{
+							for (int p = 0; p < 3; p++)
+							{
+								if (!(q == 1 && p == 1) && arr[q][p] == 'O')
+								{
+									bufer_i = q; //запоминаем индексы "Соседа"
+									bufer_j = p;
+
+									bufer_i = 1 - bufer_i; //находим "шаг"
+									bufer_j = 1 - bufer_j;
+									bufer_i = 1 + bufer_i; //находим индексы "недостоющей до тройки" ячейки
+									bufer_j = 1 + bufer_j;
+									if (arr[bufer_i][bufer_j] == ' ')//если эта ячейка пустая, то запоняем
+									{
+										arr[bufer_i][bufer_j] = 'X';
+										return 1;
+									}
+								}
+							}
+						}
+					}
+
+					if (i != 1 && j != 1 && arr[i][j] == 'O') //если Х в углу, то ищем пару
+					{
+						int count = 0; //cчетчик кол-ва угловых элементов
+
+						for (int q = 0; q < 3; q += 2) //считаем кол-во угловых элементов
+							for (int p = 0; p < 3; p += 2)
+								if (arr[q][p] == 'O')
+									count++;
+
+						if (count > 1) // если элементов 2 и больше, то ищем пары
+						{
+							if ((arr[0][0] == 'O' && arr[2][2] == 'O') || (arr[0][2] == 'O' && arr[2][0] == 'O')) //если пара диагональна
+							{
+								if (arr[1][1] == ' ') // и центр свободен, то ставим в центр
+								{
+									arr[1][1] = 'X';
+									return 1;
+								}
+							}
+
+							//4 варианта соседих углов
+							// если есть пара и элемент между ними свободен, то делаем этот ход
+							if (arr[0][0] == 'O' && arr[0][2] == 'O' && arr[0][1] == ' ')
+							{
+								arr[0][1] = 'X';
+								return 1;
+							}
+							if (arr[0][0] == 'O' && arr[2][0] == 'O' && arr[1][0] == ' ')
+							{
+								arr[1][0] = 'X';
+								return 1;
+							}
+							if (arr[2][0] == 'O' && arr[2][2] == 'O' && arr[2][1] == ' ')
+							{
+								arr[2][1] = 'X';
+								return 1;
+							}
+							if (arr[0][2] == 'O' && arr[2][2] == 'O' && arr[1][2] == ' ')
+							{
+								arr[1][2] = 'X';
+								return 1;
+							}
+						}
+					}
+
+					if ((arr[1][0] == 'O' && arr[1][2] == 'O') || (arr[0][1] == 'O' && arr[2][1] == 'O')) //если пара противоположная
+					{
+						if (arr[1][1] == ' ') // и центр свободен, то ставим в центр
+						{
+							arr[1][1] = 'X';
+							return 1;
+						}
+					}
+
+					//8 ситуаций на соседние элементы
+					if (arr[0][0] == 'O' && arr[0][1] == 'O' &&  arr[0][2] == ' ')
+					{
+						arr[0][2] = 'X';
+						return 1;
+					}
+					if (arr[0][1] == 'O' && arr[0][2] == 'O' &&  arr[0][0] == ' ')
+					{
+						arr[0][0] = 'X';
+						return 1;
+					}
+					if (arr[0][2] == 'O' && arr[1][2] == 'O' &&  arr[2][2] == ' ')
+					{
+						arr[2][2] = 'X';
+						return 1;
+					}
+					if (arr[1][2] == 'O' && arr[2][2] == 'O' &&  arr[0][2] == ' ')
+					{
+						arr[0][2] = 'X';
+						return 1;
+					}
+					if (arr[2][2] == 'O' && arr[2][1] == 'O' &&  arr[2][0] == ' ')
+					{
+						arr[2][0] = 'X';
+						return 1;
+					}
+					if (arr[2][0] == 'O' && arr[2][1] == 'O' &&  arr[2][2] == ' ')
+					{
+						arr[2][2] = 'X';
+						return 1;
+					}
+					if (arr[0][0] == 'O' && arr[1][0] == 'O' &&  arr[2][0] == ' ')
+					{
+						arr[2][0] = 'X';
+						return 1;
+					}
+					if (arr[1][0] == 'O' && arr[2][0] == 'O' &&  arr[0][0] == ' ')
+					{
+						arr[0][0] = 'X';
+						return 1;
+					}
+
+					srand(time(NULL)); //если не в рассматриваемой ситуации, то рандомно
+					bool complete = false;
+					while (!complete)
+					{
+						bufer_i = rand() % 3;
+						bufer_j = rand() % 3;
+
+						if (arr[bufer_i][bufer_j] == ' ')
+						{
+							arr[bufer_i][bufer_j] = 'X';
+							complete = true;
+						}
+					}
+					return 1;
+				}
+			}
+		}
+
+		srand(time(NULL)); //если не в рассматриваемой ситуации, то рандомно
+		bool complete = false;
+		while (!complete)
+		{
+			bufer_i = rand() % 3;
+			bufer_j = rand() % 3;
+
+			if (arr[bufer_i][bufer_j] == ' ')
+			{
+				arr[bufer_i][bufer_j] = 'X';
+				complete = true;
+			}
+		}
+		return 1;
+	}
+
 };
 
 
@@ -659,6 +971,37 @@ public:
 		} while (error);
 	}
 
+	int final_menu()
+	{
+		string choice;
+		bool error;
+
+		do
+		{
+			error = false;
+
+			cout << "Вы можете:\n"
+				<< "1. Играть еще раз\n"
+				<< "2. Перейти в меню\n"
+				<< "3. Завершить игру\n"
+				<< "Выберете желаемый вариант: ";
+			cin >> choice;
+
+			if (choice == "1")
+				return 1;
+			else if (choice == "2")
+				return 2;
+			else if (choice == "3")
+				return 3;
+
+			error = true;
+
+			system("cls");
+			cout << "Ошбика при выборе!\n" << endl;
+
+		} while (error);
+	}
+
 	void result_info(char* result)
 	{
 		switch (*result)
@@ -675,48 +1018,26 @@ public:
 		}
 	}
 
-	int repeat_or_end()
-	{
-		string choice;
-		bool error;
-
-		do
-		{
-			error = false;
-
-			cout << "Вы можете\n"
-				<< "1. Перейти в меню\n"
-				<< "2. Завершить игру\n"
-				<< "Выберете желаемый вариант: ";
-			cin >> choice;
-
-			if (choice == "1")
-				return 1;
-			else if (choice == "2")
-				return 2;
-
-			error = true;
-
-			system("cls");
-			cout << "Ошбика при выборе!\n" << endl;
-
-		} while (error);
-	}
 };
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	Draw game;
-	char* result = new char;
-	
+	char* result = new char; // победа/поражение/ничья
+	int game_mode = 0;		 // режим игры
+	int game_mode_copy = 0;	 //копия режима игры
+
 	do
 	{
 		game.logic.zeroing(); // обнуляем массив
-		//открываем меню
-		if (game.start_menu() == 1)// с компьютером
+		
+		if(game_mode == 0)
+			game_mode = game.start_menu(); //открываем меню
+
+		if (game_mode == 1)// если игра с компьютером
 		{
-			if (game.move_menu() == 1)
+			if (game.move_menu() == 1) // если ходишь первым
 			{
 				game.map();
 				while (!game.logic.full(result) && !game.logic.win(result)) // пока "не победа" и есть ячейки
@@ -726,25 +1047,25 @@ int main()
 					Sleep(500);
 					if (!game.logic.full(result) && !game.logic.win(result)) // если не победа" и есть ячейки
 					{
-						if (game.logic.attack() == 2)// если атака не выполнилась, то выполняем защиту
-							game.logic.defense();
+						if (game.logic.attack_O() == 2)// если атака не выполнилась, то выполняем защиту
+							game.logic.defense_O();
 						game.map();
 					}
 				}
 			}
-			else ///////////////////
+			else //если ходишь 2ым
 			{
 				game.map();
 				while (!game.logic.full(result) && !game.logic.win(result)) // пока "не победа" и есть ячейки
 				{
 					Sleep(500);
-					if (game.logic.attack() == 2)// если атака не выполнилась, то выполняем защиту
-						game.logic.defense();
+					if (game.logic.attack_X() == 2)// если атака не выполнилась, то выполняем защиту
+						game.logic.defense_X();
 					game.map();
 
-					if (!game.logic.full(result) && !game.logic.win(result)) // если не победа" и есть ячейки
+					if (!game.logic.full(result) && !game.logic.win(result)) // если "не победа" и есть ячейки
 					{
-						game.logic.Enter_X();
+						game.logic.Enter_O();
 						game.map();
 					}
 				}
@@ -758,7 +1079,7 @@ int main()
 				game.logic.Enter_X();
 				game.map();
 
-				if (!game.logic.full(result) && !game.logic.win(result)) // если не победа" и есть ячейки
+				if (!game.logic.full(result) && !game.logic.win(result)) // если "не победа" и есть ячейки
 				{
 					game.logic.Enter_O();
 					game.map();
@@ -766,7 +1087,12 @@ int main()
 			}
 		}
 		game.result_info(result);
-	} while (game.repeat_or_end() == 1);
+
+		game_mode_copy = game.final_menu();
+		if (game_mode_copy == 2) // если выбрали "меню", то обнулить режим игры
+			game_mode = 0;
+
+	} while (game_mode_copy != 3);
 
 	delete result;
 	return 0;
